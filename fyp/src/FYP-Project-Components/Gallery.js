a2390a147417c7acd1a1dfbe1b5e8b10bb53a5b6
muhammadBilal056 <109471675+muhammadBilal056@users.noreply.github.com>
2022-12-29 00:15:00 +0500

    update heading

--- fyp/src/FYP-Project-Components/Gallery.js
+++ fyp/src/FYP-Project-Components/Gallery.js
@@ -6,7 +6,7 @@ const Gallery = () => {
   return (
     <Box>
       <Box sx={{width: "83px", height: "83px", textAlign: "center"}}> Navbar</Box>
-      <Typography sx={{pt: "110px"}} component="h2" variant="h2">My Project</Typography>
+      <Typography sx={{pt: "55px"}} component="h2" variant="h2">My Project</Typography>
       <Divider sx=
       {{mt: "50px", mb: "50px", color: "black", borderColor: "#434F53",
        background: "transparent"}} variant="middle"/>
