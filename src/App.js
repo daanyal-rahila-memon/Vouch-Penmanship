d7caabf62edad5e8219226de9bd8f4b54adc865b
muhammadBilal056 <109471675+muhammadBilal056@users.noreply.github.com>
2023-01-07 19:15:12 +0500

    Changed Repository and Added some file

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React from "react";
+import './App.css';
+import Card from "./React-Project/Card"
+import data from "./React-Project/CardArray"
+
+import NFTCard from "./FYP-Project-Components/NFTCard";
+import Login2 from "./FYP-Project-Components/Login2"
+import Signup from "./FYP-Project-Components/Signup";
+
+
+import Gallery from "./FYP-Project-Components/Gallery";
+import ForgotPassword from "./FYP-Project-Components/ForgetPassword";
+
+import {Routes, Route} from "react-router-dom";
+
+function App() {
+
+const Element = [{id:1, rating: "5.0", reviewCount: 8, country: "pakistan",
+title: "Life With jony jony", price: 16},
+{id: 2, rating: "5.0", reviewCount: 8, country: "pakistan",
+title: "Life With Honey Honey", price: 16},
+{id:3, rating: "5.0", reviewCount: 8, country: "pakistan",
+title: "Life With jony jony", price: 16}]
+
+const ProfileDetails = [{id: 1, value: "Technology", techImg: "technology.png"},
+{id: 2, value: "Description", techImg: "technology.png"},
+{id: 3, value: "Supervisor", techImg: "technology.png"},
+{id: 4, value: "Supervisor", techImg: "technology.png"}]
+
+const ElementArray = Element.map(data => {
+  return <Card
+  key={data.id}
+  // item={data}
+  {...data}
+   />
+})
+
+// const list = ProfileDetails.map(data => <Button>{data.value}</Button>)
+
+const cardArray = data.map(items => {
+  return <Card
+  key={items.id}
+  {...items}
+   />
+})
+
+const NFTCardArray = []
+for (var i=0; i<12; i++)
+{
+  NFTCardArray.push(<NFTCard />)
+}
+
+<div className="App">
+{/* {ElementArray}
+<Navbar />
+{cardArray} */}
+{/* {<Login /> */}
+
+
+
+
+{/* <Signup /> */}
+
+{/* <UserProfile value={ProfileDetails}/> */}
+
+{/* <Login2 /> */}
+
+</div>
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login2 />}/>
+      <Route path="/signUp" element={<Signup />} />
+      <Route path="/Gallery" element={<Gallery />}/>
+      <Route path="/ForgetPassword" element={<ForgotPassword />} />
+    </Routes>
+  );
+}
+
+
+
+export default App;
---
+++ src/FYP-Project-Components/Gallery.js
@@ -0,0 +1,87 @@
+import {Button, Box, Typography, Divider, Pagination, Stack } from "@mui/material"
+import NFTCard from "./NFTCard"
+import Grid from "@mui/material/Unstable_Grid2/Grid2"
+import {motion} from "framer-motion"
+import React, { useEffect, useState } from "react"
+
+
+const Gallery = () => {
+  var i = 1;
+  const [dis, setDis] = useState([0])
+  const supervisorList = ["Rizwan-ul-Haq", "Muhammad Bilal", "Furqan Athar",
+  "Kashif Aziz", "Salman Aziz", "Umer Hayat", "Umair Akram", "Muhammad Rizwan",
+  "Zubair Sattar", "Muhammad Uzair", "Ali Hamza", "Waseem Akram"]
+
+  var NFTCardArray = []
+
+  // NFTCardArray.push(<motion.div initial={{x:-100}} whileInView={{x: 5}} transition={{
+  //   type: "spring",
+  //   stiffness: 70,  
+  //   damping: 20
+  // }}><NFTCard supervisorName={supervisorList[i]}/></motion.div>)
+
+  
+
+  // const [nftCardArray, setNFTCardArray] = useState([<motion.div initial={{x:-100}} whileInView={{x: 5}} transition={{
+  //   type: "spring",
+  //   stiffness: 70,  
+  //   damping: 20
+  // }}><NFTCard supervisorName={supervisorList[i]}/></motion.div>])
+
+
+ 
+  // const NFTCardArray = []
+
+  // useEffect(() => {
+
+  //   if (dis.length < 12)
+  //   {
+  //     setTimeout(() => {
+  //       setDis(value => [...value, dis.length]) 
+  //       console.log(dis)
+  //     }, 1000)
+  //   }
+  // }, [dis, nftCardArray])
+
+
+  // const Display = () => {
+
+  //   return nftCardArray
+  // }
+  
+  for (var i=0; i<12 ; i++)
+  {
+    // NFTCardArray.push(<motion.div initial={{x:-100}} whileInView={{x: 5}} transition={{
+    //   type: "spring",
+    //   stiffness: 70,
+    //   damping: 20,
+    // }}><NFTCard supervisorName={supervisorList[i]} wait={1000*i}/></motion.div>)
+
+    NFTCardArray.push(<NFTCard supervisorName={supervisorList[i]} wait={i}/>)
+  }
+
+  return (
+    <>
+      <Box>
+        <Box sx={{width: "83px", height: "83px", textAlign: "center", position: "fixed"}}> Navbar</Box>
+        <Typography sx={{pt: "55px", textAlign: "center"}} component="h2" variant="h2">My Project</Typography>
+        <Divider sx=
+        {{mt: "50px", mb: "50px", color: "black", borderColor: "#434F53",
+        background: "transparent"}} variant="middle"/>
+      </Box>
+      <Grid spacing={4} container justifyContent="center">
+        
+       {NFTCardArray}
+        {/* {<Profile /> } */}
+      </Grid>
+
+
+      {/* <Stack>
+        <Pagination onClick={()=> {}} count={NFTCardArray.length/6} />
+      </Stack> */}
+      
+    </>
+  )
+}
+
+export default Gallery
