8d8b40db5612d9ec493228a557b56e846e898707
Muhammad Daanyal <daanyalhere@gmail.com>
2023-01-30 22:42:12 +0500

    Made a controller
    - Made a controller - in /controllers/student.js - to get all the students from the database

--- proj_backend/routes/student.js
+++ proj_backend/routes/student.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { isSignedIn, isAuthenticated, isAdmin, isSupervisor } = require("../controllers/auth"); // things to be imported from controllers/auth
-const { getStudentById, getStudent } = require("../controllers/student"); // things to be imported from controllers/student
+const { getStudentById, getStudent, getAllStudents } = require("../controllers/student"); // things to be imported from controllers/student
 
 router.param("studentId", getStudentById);
 router.get("/student/:studentId", isSignedIn, isAuthenticated, getStudent);
+// router.get("/getAllStudents", getAllStudents); // get all students
 
 module.exports = router;
