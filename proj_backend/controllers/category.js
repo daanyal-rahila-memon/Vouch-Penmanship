532a37349a012982d864a1c2ddcad52612adf764
Muhammad Daanyal <daanyalhere@gmail.com>
2023-03-27 13:17:09 +0500

    Work over Category Model, Route & Controller

--- proj_backend/controllers/category.js
+++ proj_backend/controllers/category.js
@@ -12,19 +12,22 @@ exports.getCategoryById = (req, res, next, id) => {
   });
 };
 
+// Create
 exports.createCategory = (req, res) => {
+  req.body.name = req.body.name.toLowerCase(); // to convert the user's email in Lower Case
   const category = new Category(req.body);
 
   category.save((error, category) => {
     if (error || !category) {
       return res.status(400).json({
-        error: "Category not found in DB",
+        error: "NOT able to save category in DB",
       });
     }
-    return res.json({ category });
+    return res.json(category);
   });
 };
 
+// Read
 exports.getCategory = (req, res) => {
   return res.json(req.category);
 };
@@ -36,6 +39,37 @@ exports.getAllCategories = (req, res) => {
         error: "No categories found",
       });
     }
-    res.json(categories);
+    return res.json(categories);
+  });
+};
+
+// Update
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+
+  category.save((error, updatedCategory) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Failed to update category",
+      });
+    }
+    return res.json(updatedCategory);
+  });
+};
+
+// Delete
+exports.deleteCategory = (req, res) => {
+  const category = req.category;
+
+  category.remove((error, category) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Failed to delete category",
+      });
+    }
+    return res.json({
+      Message: `${category} Successfully deleted`,
+    });
   });
 };
